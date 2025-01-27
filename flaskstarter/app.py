f0fe2a3e5aadf6670455760ac9f84b181225f044
Salifou Diallo <diallosalifou86@gmail.com>
2025-01-27 12:06:13 -0500

    update

--- flaskstarter/app.py
+++ flaskstarter/app.py
@@ -3,12 +3,7 @@
 from flask import Flask
 
 from .config import DefaultConfig
-from .user import Users, UsersAdmin
-from .settings import settings
-from .tasks import tasks, MyTaskModelAdmin
-from .frontend import frontend, ContactUsAdmin
-from .extensions import db, mail, cache, login_manager, admin
-from .utils import INSTANCE_FOLDER_PATH, pretty_date
+from .extensions import db
 
 
 # For import *
@@ -22,7 +17,6 @@ def create_app(config=None, app_name=None):
         app_name = DefaultConfig.PROJECT
 
     app = Flask(app_name,
-                instance_path=INSTANCE_FOLDER_PATH,
                 instance_relative_config=True)
 
     return app
--- flaskstarter/extensions.py
+++ flaskstarter/extensions.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_caching import Cache
-from flask_login import LoginManager
-from flask_admin import Admin, AdminIndexView
-from flask_admin.menu import MenuLink
 
-
-db = SQLAlchemy()
-
-cache = Cache()
+db = None
