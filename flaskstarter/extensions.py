5a78e32cf9790e5abac1b485d69942d0e3dd75f8
p6nj <breee@duck.com>
2025-01-27 11:47:42 -0500

    cleanup

--- flaskstarter/extensions.py
+++ flaskstarter/extensions.py
@@ -10,24 +10,4 @@ from flask_admin.menu import MenuLink
 
 db = SQLAlchemy()
 
-mail = Mail()
-
 cache = Cache()
-
-login_manager = LoginManager()
-login_manager.login_view = "frontend.login"
-login_manager.login_message = "Please login or signup to add a new trip"
-
-
-class HomeView(AdminIndexView):
-    def is_visible(self):
-        return False
-
-# Config default flask-admin view
-
-
-admin = Admin(name='Flask-Starter Admin', template_mode='bootstrap3', index_view=HomeView(name='Home'))
-
-admin.add_link(MenuLink(name='Back to Dashboard', url='/dashboard', icon_type='glyph', icon_value='glyphicon-circle-arrow-left'))
-
-admin.add_link(MenuLink(name='Logout', url='/logout', icon_type='glyph', icon_value='glyphicon-log-out'))
